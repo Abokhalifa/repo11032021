b2b9232934e3d6b1b1d7748e42d49e458cb7d804
Abokhalifa <45393846+Abokhalifa@users.noreply.github.com>
2021-03-12 21:22:28 +0200

    Update Operations.java
    Add a comment for considering defensive programming for non-integers.

--- src/main/java/app/Operations.java
+++ src/main/java/app/Operations.java
@@ -3,7 +3,7 @@ package app;
 public class Operations {
 	
 	public int addTwoIntigers(int a, int b) {
-		return a+b;
+		return a+b; //The parameters must be integer so, defensive programming ought to apply here.
 	}
 
 }
